06ac1d2990297d3eba84aea2805c518f114e1dae
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2022-01-07 13:32:56 +0100

    Session 13

---
+++ ReactJS/proyect-task/src/pages/tasks/TaskPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TaskListComponent from '../../component/container/task_list';
+
+const TaskPage = () => {
+  return (
+    <div>
+      <TaskListComponent />
+    </div>
+  )
+}
+
+export default TaskPage
